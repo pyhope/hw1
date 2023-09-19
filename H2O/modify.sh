91c7966cad00c5571e20cffa372d293757f9ab07
Yihang Peng <ypeng0082@gmail.com>
2023-09-19 17:57:58 -0400

    update

--- H2O/modify.sh
+++ H2O/modify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 WDIR=`pwd`
-for i in 200  300  400  500  600  700
+for i in 200  300  400  500  600  700 800 900 1000
 do
 cd $WDIR/$i
 # sed -i "s/--job-name=\$i/--job-name=$i/g" run.slurm
 # sed -i "s/ENCUT = \$i/ENCUT = $i/g" INCAR
-bash cleanup.sh
+# bash cleanup.sh
 sbatch run.slurm
 done
